028bf88d2a9fd9bdbc21eb31fc1f79feb82a4bed
Michael Basil <michael@intrications.com>
2013-03-29 22:32:17 +0000

    Do not try to use custom font in Eclipse Layout Editor (requires API level 3)

--- src/com/devspark/robototextview/widget/RobotoTextView.java
+++ src/com/devspark/robototextview/widget/RobotoTextView.java
@@ -117,12 +117,14 @@ public class RobotoTextView extends TextView {
      * @param attrs   The attributes of the XML tag that is inflating the widget.
      */
     private void parseAttributes(Context context, AttributeSet attrs) {
-        TypedArray values = context.obtainStyledAttributes(attrs, R.styleable.RobotoTextView);
+    	if (!isInEditMode()) {
+    		TypedArray values = context.obtainStyledAttributes(attrs, R.styleable.RobotoTextView);
 
-        int typefaceValue = values.getInt(R.styleable.RobotoTextView_typeface, 0);
-        values.recycle();
+        	int typefaceValue = values.getInt(R.styleable.RobotoTextView_typeface, 0);
+        	values.recycle();
 
-        setTypeface(obtaintTypeface(context, typefaceValue));
+        	setTypeface(obtaintTypeface(context, typefaceValue));
+    	}
     }
 
     /**
