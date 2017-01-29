77818475b68dfb71ee5579ca4d5cbdabb2a8f25c
OlegKrikun <oleg.krikun@gmail.com>
2017-01-29 12:16:14 +0300

    Add plugin order and extension test

--- robototextview-gradle-plugin/src/test/groovy/com/devspark/robototextview/gradle/RobotoTextViewPluginTest.groovy
+++ robototextview-gradle-plugin/src/test/groovy/com/devspark/robototextview/gradle/RobotoTextViewPluginTest.groovy
@@ -4,6 +4,7 @@ import com.android.build.gradle.AppPlugin
 import com.android.build.gradle.LibraryPlugin
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Assert
 import org.junit.Test
 
 class RobotoTextViewPluginTest {
@@ -40,5 +41,43 @@ class RobotoTextViewPluginTest {
     void testApplyWithoutAndroidPlugin() {
         Project project = ProjectBuilder.builder().build()
         project.plugins.apply(RobotoTextViewPlugin)
+    }
+
+    @Test
+    void testApplyPluginOrder() {
+        Project project = ProjectBuilder.builder().build()
+        project.buildscript.repositories {
+            jcenter()
+        }
+        project.buildscript.dependencies {
+            classpath 'com.android.tools.build:gradle:2.2.0'
+        }
+
+        project.plugins.apply(RobotoTextViewPlugin)
+        project.plugins.apply(AppPlugin)
+    }
+
+    @Test
+    void testExtension() {
+        Project project = ProjectBuilder.builder().build()
+        project.buildscript.repositories {
+            jcenter()
+        }
+        project.buildscript.dependencies {
+            classpath 'com.android.tools.build:gradle:2.2.0'
+        }
+
+        project.plugins.apply(AppPlugin)
+        project.plugins.apply(RobotoTextViewPlugin)
+
+        project.robototextview {
+            include 'include1', 'include2'
+            exclude 'exclude1', 'exclude2'
+            log true
+        }
+
+        Assert.assertTrue(project.robototextview.include == ['include1', 'include2'])
+        Assert.assertTrue(project.robototextview.exclude == ['exclude1', 'exclude2'])
+        Assert.assertTrue(project.robototextview.log)
     }
 }
