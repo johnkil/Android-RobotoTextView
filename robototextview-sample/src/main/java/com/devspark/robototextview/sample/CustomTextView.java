769cab33a7e3f8d69221c2f88b9542eb3c2df702
Max Rovkin <rovkinmax@gmail.com>
2017-02-13 16:36:25 +0500

    fixed ignore custom view

---
+++ robototextview-sample/src/main/java/com/devspark/robototextview/sample/CustomTextView.java
@@ -0,0 +1,25 @@
+package com.devspark.robototextview.sample;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+
+public final class CustomTextView extends TextView {
+    public CustomTextView(Context context) {
+        super(context);
+    }
+
+    public CustomTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CustomTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
